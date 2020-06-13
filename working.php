e9b3d418f6b7d155c405986bee6746b9679d7e5f
Peter Bowyer <peter@mapledesign.co.uk>
2020-06-13 17:49:36 +0100

    Initial commit

---
+++ working.php
@@ -0,0 +1,51 @@
+<?php
+
+require __DIR__.'/vendor/autoload.php';
+
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\SingleCommandApplication;
+use Symfony\Component\HttpClient\HttpClient;
+
+(new SingleCommandApplication())
+    ->setCode(
+        function (InputInterface $input, OutputInterface $output) {
+            $client = HttpClient::create();
+
+            $responses = [];
+            // Do the backend request
+            $responses[] = $client->request(
+                'POST',
+                'http://mockbin.org/request',
+                [
+                    'body' => [
+                        'success' => 'yes',
+                    ],
+                ]
+            );
+            $responses[] = $client->request(
+                'POST',
+                'http://mockbin.org/status/400/Bad+Request',
+                [
+                    'body' => [
+                        'success' => 'no',
+                    ],
+                ]
+            );
+            foreach ($client->stream($responses) as $response => $chunk) {
+                if ($chunk->isFirst()) {
+                    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                    // @HELP If I uncomment these lines, it works.
+                    // If I don't, an exception is thrown.
+                    if ($response->getStatusCode() != 200) {
+
+                    }
+                    //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+                } elseif ($chunk->isLast()) {
+                    $json = $response->toArray(false);
+                    dump($json);
+                }
+            }
+        }
+    )
+    ->run();
